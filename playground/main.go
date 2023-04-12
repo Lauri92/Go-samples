3688039cf0a25934e46e5f93f22d967be1579fd5
Lauri Riikonen <lauri92.riikonen@gmail.com>
2023-04-12 20:04:11 +0300

    Structs and pointers

--- playground/main.go
+++ playground/main.go
@@ -4,13 +4,15 @@ import "fmt"
 
 func main() {
 
-	list := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	list := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 16}
 
-	for i := range list {
-		if i%2 == 0 {
-			fmt.Printf("%v is odd\n", i)
+	list = append(list, 11)
+
+	for _, number := range list {
+		if number%2 == 0 {
+			fmt.Println(number, "is even")
 		} else {
-			fmt.Printf("%v is not odd\n", i)
+			fmt.Printf("%v is odd\n", number)
 		}
 	}
 
